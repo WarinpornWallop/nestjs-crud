52cbc29ed5b7f6fab29d1cff55a29b32895f1691
Warinporn Walloppornlapak <warinporn.wall@gmail.com>
2025-07-23 21:20:21 +0700

    ADD: user

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URL ?? '', {
+      dbName: process.env.DBNAME,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
